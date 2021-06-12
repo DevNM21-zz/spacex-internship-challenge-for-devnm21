8c2202424e7716921838f3d37fcf66febbd80e27
dev <pydev21@gmail.com>
2021-06-12 19:17:10 +0530

    Add dashboard page (home page)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Header from '../src/components/Header';
+import Container from 'reactstrap/lib/Container';
+import Dashboard from './pages/Dashboard';
+import './App.css';
+
 function App() {
   return (
     <div>
       <Header />
+      <Container className={'main'}>
+        <Dashboard />
+      </Container>
     </div>
   );
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import SpacexTable from '../components/Table';
+
+const Dashboard = () => {
+	return (
+		<>
+			<SpacexTable />
+		</>
+	)
+}
+
+export default Dashboard;

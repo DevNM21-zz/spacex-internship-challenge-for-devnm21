0f1fdf2f20eca1675c290a8d59c91c8d9f9c2f4f
dev <pydev21@gmail.com>
2021-06-12 19:16:49 +0530

    create custom table component (spacex table)

---
+++ src/components/Table.js
@@ -0,0 +1,20 @@
+import Table from 'reactstrap/lib/Table';
+import './Table.css'
+const SpacexTable = () => {
+	return (
+		<Table>
+			<thead>
+			<td>No:</td>
+			<td>Launched (UTC)</td>
+			<td>Location</td>
+			<td>Mission</td>
+			<td>Orbit</td>
+			<td>Launch Status</td>
+			<td>Rocket</td>
+			</thead>
+			<tbody>
+			</tbody>
+		</Table>
+	)
+}
+export default SpacexTable;
